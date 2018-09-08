3b176930646a14f8f488fd2b23117fa195ba5775
braj011 <bhagyaraj01@gmail.com>
2018-09-08 18:03:09 +0100

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,24 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative 'fancy_dance'
+
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+# refer to the name-spaced modules or classes with ::. This syntax
+# references the parent and child relationship of the nested modules
+
+#  extend MetaDancing
+
+
+  #include Dance
+  # If we use include keyword, we allow our classes to use all of
+  # the methods of the included module as instance methods.
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
